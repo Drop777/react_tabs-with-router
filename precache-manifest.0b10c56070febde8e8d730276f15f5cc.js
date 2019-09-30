e22a9a88e2aec3ce8105a276c4758a39ccbc03eb
Pukhnichenko <victorpuhnichenko@gmail.com>
2019-10-01 00:20:39 +0300

    Updates

---
+++ precache-manifest.0b10c56070febde8e8d730276f15f5cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a8e129cf489042625fe1fd01684100d",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "996b95c50df888684e57",
+    "url": "/react_tabs-with-router/static/css/main.6b171aae.chunk.css"
+  },
+  {
+    "revision": "105768a5891750b3fe7d",
+    "url": "/react_tabs-with-router/static/js/2.0938cfc9.chunk.js"
+  },
+  {
+    "revision": "996b95c50df888684e57",
+    "url": "/react_tabs-with-router/static/js/main.c22388b0.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
